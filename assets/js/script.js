749d0a6faf785aef355326dab6b1813a7dd1c876
bennettem <emily.pamelabennett@gmail.com>
2022-03-14 22:36:16 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var buttonEL = document.querySelector("#save-task");
+var tasksToDoEL = document.querySelector("#tasks-to-do");
+
+// buttonEL.addEventListener("click", createTaskHandler) {
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+//   listItemEl.textContent = "This is a new task.";
+//   tasksToDoEL.appendChild(listItemEl);
+// };
+
+var createTaskHandler = function () {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEL.appendChild(listItemEl);
+};
+
+buttonEL.addEventListener("click", createTaskHandler);
